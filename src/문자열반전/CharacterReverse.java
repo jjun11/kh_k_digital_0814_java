cf695679355286347ab6146917b4fa837c4dc3d7
jhj2023 <gus95wns@naver.com>
2023-08-18 17:39:43 +0900

    ddddddddf

---
+++ src/문자열반전/CharacterReverse.java
@@ -0,0 +1,19 @@
+package 문자열반전;
+
+import java.util.Scanner;
+
+// 문자열을 입력받음 : abcd -> dcba
+// 문자열의 길이는 length()로 확인 가능
+//
+public class CharacterReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열 입력 : ");
+        String input = sc.next();
+        int pos = input.length() - 1; // 문자열의 시작 위치(반전의 위치)
+        while (pos > -1) {
+            System.out.print(input.charAt(pos));
+            pos--;
+        }
+    }
+}
